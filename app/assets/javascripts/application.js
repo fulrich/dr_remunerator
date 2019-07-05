81aa8612e35342ed94c686adb32ebe1fa8d23341
Fred Ulrich <fred.ulrich@shopify.com>
2019-07-05 15:25:40 -0400

    Adding a simple style for the project.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//
+//= require vendor/jquery/jquery.min.js
+//= require vendor/bootstrap/js/bootstrap.bundle.min.js
+//= require vendor/jquery-easing/jquery.easing.min.js
+//= require js/sb-admin-2.js
+

61f0ea659233b751e55dd04c313d8a55de5f7eef
Fred Ulrich <fred.ulrich@shopify.com>
2019-07-11 15:34:12 -0400

    Moving the last secret over, hope this shit works.

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    ENV["AUTH0_CLIENT_ID"],
+    Rails.application.secrets.auth0_client_id,
     Rails.application.secrets.auth0_client_secret,
     Rails.application.secrets.auth0_domain,
     callback_path: '/auth/oauth2/callback',
